315b9b2ef81bfad20eb0983495e1c76f4579300d
Tushar-Hegde <128315293+Tushar-Hegde@users.noreply.github.com>
2023-09-24 08:56:34 +0530

    Add files via upload

---
+++ forums/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from . import views
+
+app_name='forums'
+
+urlpatterns = [
+    path('forums/<str:forum_id>',views.forum,name='forum'),
+    path('event/<int:event_id>',views.event,name='event'),
+    path('forums/<int:forum_id>/members',views.member_list,name='member_list'),
+    path('makeforum',views.makeforum,name='makeforum'),
+    path('test', views.test, name='test')
+]
+
